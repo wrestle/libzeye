7c7789448dc18615fbaf1c719f0d3822df2a8445
WuShengxin <wsxinchina@gmail.com>
2016-04-26 21:02:23 +0800

    Fix

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "test/test.h"
 
 int main(int argc, char* argv[]) {
-    user_t tmp = make_users("wsxinchina@gmail.com", "WSX88619973wsx");
+    user_t tmp = make_users("", "");
     //test_res_info(tmp); /* Pass */
     test_host_search(tmp); /* Pass */
     //test_web_search(tmp); /* Pass */
